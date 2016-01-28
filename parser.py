320ebf7fa47af0e1bfe6c7b5ef5c0a13b1a05ef3
Elizaveta Ivtushok <springbang@gmail.com>
2016-01-28 22:47:10 +0300

    _
    -

--- parser.py
+++ parser.py
@@ -8,7 +8,7 @@ month = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
 
 for i in month:
     result = re.findall ('http://tema.livejournal.com/2015/'+ i + '/[0-9]+', homepage_text)
-    text_output = open(i + '.txt', 'w', encoding = 'utf-8')
+    text_output = open('output.txt', 'w', encoding = 'utf-8')
     for j in result:
         postpage = urllib.request.urlopen(j)
         stringpostpage=postpage.read()
