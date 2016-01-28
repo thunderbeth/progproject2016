7d21c1d97748087e78911411872f0097b24e0741
Ozyrus <imbarus@gmail.com>
2016-01-28 03:47:33 +0300

    Тащит голый хороший текст. Чтобы работало накатите красивыйсуп
    Нахрен етри, делаем все супом.
    Тащит голенький как попка младенца текст, вам осталось только распихать
    его по файлам как вам надо.
    TRIGGER WARNING: много мата
    сука, блядь, я два ебаных часа пытался определить под каким сука тегом
    валяется нужный текст, кликая по ссылкам на страничке с индексом, а
    оказалось что ваш ебаный парсер тащит СТАРЫЕ ХТМЛ СТРАНИЧКИ ГДЕ ВСЕ
    ПРОСТО И ХОРОШО
    СУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУКААААААААААААААААААААААААААААААААААААААААА
    О ТАКОМ НАДО ГОВОРИТЬ БЛЯДЬ В ПЕРВУЮ СУКА ОЧЕРЕДЬ
    REEEEEEEEEEEEEEEEEEEEEEEE

--- parser.py
+++ parser.py
@@ -1,21 +1,21 @@
 import urllib.request
 import re
-from lxml import etree
-
+from bs4 import BeautifulSoup
 text_output = open('output.txt', 'w', encoding = 'utf-8')
 homepage = urllib.request.urlopen('http://tema.livejournal.com/2015/')
 homepage_text = homepage.read().decode('utf-8')
 
 month = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
 
-
 for i in month:
     result = re.findall ('http://tema.livejournal.com/2015/'+ i + '/[0-9]+', homepage_text)
     for j in result:
         postpage = urllib.request.urlopen(j)
         stringpostpage=postpage.read()
-        print(stringpostpage)
-        tree = etree.HTML(stringpostpage)
-        for i in tree.xpath('//*[@class="subject"]'):
-            print (i)
-        
+        print(j)
+        soup = BeautifulSoup(stringpostpage, "lxml")
+        texts = soup.findAll('td',class_ ='entry')
+        for i in texts:
+            cleantext = BeautifulSoup.get_text(i, separator=u' ')
+            cleantext = re.sub('Tags:.*?Share','',cleantext)
+            print(cleantext)
