517714ffda6e5fc98137ab6820790f1d45eff7e6
Elizaveta Ivtushok <springbang@gmail.com>
2016-01-28 20:20:12 +0300

    Почему
    гит заставляет меня писать каждый раз((

--- parser.py
+++ parser.py
@@ -1,7 +1,6 @@
 import urllib.request
 import re
 from bs4 import BeautifulSoup
-text_output = open('output.txt', 'w', encoding = 'utf-8')
 homepage = urllib.request.urlopen('http://tema.livejournal.com/2015/')
 homepage_text = homepage.read().decode('utf-8')
 
@@ -9,6 +8,7 @@ month = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
 
 for i in month:
     result = re.findall ('http://tema.livejournal.com/2015/'+ i + '/[0-9]+', homepage_text)
+    text_output = open(i + '.txt', 'w', encoding = 'utf-8')
     for j in result:
         postpage = urllib.request.urlopen(j)
         stringpostpage=postpage.read()
@@ -17,4 +17,4 @@ for i in month:
         for i in texts:
             cleantext = BeautifulSoup.get_text(i, separator=u' ')
             cleantext = re.sub('Tags:.*?Share','',cleantext)
-            print(cleantext)
+            text_output.write(cleantext)
