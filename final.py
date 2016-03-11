2a93f3badc11f27192444171ee38653a285caab7
Elizaveta Ivtushok <springbang@gmail.com>
2016-03-11 04:16:39 +0300

    Revert "bb"
    This reverts commit f31e46ef35b650d0ab267c94c46b3e321e4a7a16.

--- final.py
+++ final.py
@@ -2,19 +2,12 @@ import os
 import re
 
 #number = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
-summ == 0
 def author (path):
     for filename in path:
         profile1 = open ('01.txt_bigramy.txt', 'r', encoding = 'utf-8')
         profile2 = open ('02.txt_bigramy.txt', 'r', encoding = 'utf-8')
-        for key1, value1 in dic1:
-            for key2, value2 in dic2:
-                if key1 == key2:
-                    summ == value1 + value2
-                else:
-                    summ == value1 + 0
-                print(summ)
-                
+        for line in profile1:
+            for line in profile2:
                 
                 
         
